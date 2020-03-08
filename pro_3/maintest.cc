5f644a09bc93421059b9149f174ab87b7be9fb04
Jmartin042 <jonathan3979701@gmail.com>
2020-03-08 13:22:23 -0400

    comments

--- pro_3/maintest.cc
+++ pro_3/maintest.cc
@@ -6,10 +6,10 @@ int main ()
 				// which has 4 students each with 3 grades;
 	//g.LIST(-1); // alphabetize;
 	//g.ALPHABETIZE(1);
-	//g.VERIFY("KATE", "OGGEN"); // verify student;
+	g.VERIFY("MONALISA", "SCHONBRUN"); // verify student;
 	//g.VERIFY("JONATHAN", "RAYA"); // no such student;
 	//g.VERIFY("ASILEM", "RAYA");
-	g.REPORT(0, 0, 0, 0); // report the grade for id 3333 pin 33 exam 0;
+	//g.REPORT(0, 0, 0, 0); // report the grade for id 3333 pin 33 exam 0;
 	//g.REPORT(0, 0, 0, 0); // report all grades;
         return 0;
 }
