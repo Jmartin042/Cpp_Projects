729d09184ca5d71d7c33d4571879dbf557f9f900
Jmartin042 <jonathan397901@gmail.com>
2020-03-18 21:12:19 -0400

    new

--- pro_4/maintest.cc
+++ pro_4/maintest.cc
@@ -2,20 +2,20 @@
 // example program: main4_3.cc
 int main ()
 {
-        OVERLOADED_GRADES g1("G1", 4,3), g2("G2", 2,3), g3("G3", 0,0);  
+        OVERLOADED_GRADES g1("G1", 4,3);
 
 	//g1.OVERLOADED_LIST(1);
 	//g2.OVERLOADED_LIST(1);
 	//g3.OVERLOADED_LIST(1);
 
-	//g3 = g1 + g2;
+	g1-=4444;
 	g1.OVERLOADED_LIST(1);
-	g1 = g1+g1+g1;
+	//g1 = g1+g1+g1;
 
 	//g1.OVERLOADED_LIST(1);
 	//g2.OVERLOADED_LIST(1);
     //g3 -= 3333;
-	g1.OVERLOADED_LIST(1);
+	//g1.OVERLOADED_LIST(1);
 
         return 0;
 	
